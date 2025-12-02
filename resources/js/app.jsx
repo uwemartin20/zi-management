3122500e042584ecc7c917963978907aa5ea07b2
Hamza Mehmood <hamza.mehmood@zmwl.local>
2025-12-02 12:12:05 +0100

    added PWA application for mobile

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -53,3 +53,11 @@ createInertiaApp({
   },
   progress: false,
 });
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('SW registered:', reg))
+      .catch(err => console.log('SW registration failed:', err));
+  });
+}
